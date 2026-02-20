568c67670914ae04df002b7624acb7d2dff248d5
Shlykovpv <Shlykovpv@kamaz.ru>
2026-02-20 20:52:24 +0300

    step 3 - решение

--- main.go
+++ main.go
@@ -7,26 +7,68 @@ func askUser ()(string, string, float64) {
 	var currencyTarget string
 	var summ float64
 
-	fmt.Print("Какую валюту вы хотите поменять?")
-	fmt.Scan(&currencyChange)
-	fmt.Print("На какую валюту вы хотите сконвертировать?")
-	fmt.Scan(&currencyTarget)
-	fmt.Print("Какую сумму?")
-	fmt.Scan(&summ)
+	for {
+		fmt.Print("Какую валюту вы хотите поменять (EUR/USD/RUB): ")
+		fmt.Scan(&currencyChange)
+		if currencyChange == "EUR" || currencyChange == "USD" || currencyChange == "RUB" {
+			break
+		}
+	}
 
-	return currencyChange, currencyTarget, summ
+    for {
+	    switch currencyChange {
+	    case "EUR":
+	        fmt.Print("На какую валюту вы хотите сконвертировать (USD/RUB): ")
+		case "USD":
+			fmt.Print("На какую валюту вы хотите сконвертировать (EUR/RUB): ")
+		case "RUB":
+			fmt.Print("На какую валюту вы хотите сконвертировать (EUR/USD): ")
+		}
+		fmt.Scan(&currencyTarget)
+		if  currencyTarget=="EUR" && currencyTarget !=currencyChange || 
+	   		currencyTarget=="USD" && currencyTarget !=currencyChange || 
+	   		currencyTarget=="RUB" && currencyTarget !=currencyChange {
+		break
+		}
+	}
+
+	for {
+		fmt.Print("Введите сумму для обмена: ")
+		fmt.Scan(&summ)
+		if summ>0 {
+			break
+		}
+	}
+return currencyChange, currencyTarget, summ
 }
 
-func countCurrency (currencyChange string, currencyTarget string, summ float64) {
-
-}
-
-func main () {
+func countCurrency (currencyChange string, currencyTarget string, summ float64) float64 {
 	const exchangeUSDtoEUR float64 = 0.84
 	const exchangeUSDtoRUB float64 = 76.01
 	const exchangeEURtoRUB float64 = exchangeUSDtoRUB / exchangeUSDtoEUR
+    var result float64
+	 
+    switch {
+	case currencyChange=="EUR" && currencyTarget=="USD":
+		result=summ/exchangeUSDtoEUR
+	case currencyChange=="EUR" && currencyTarget=="RUB":
+		result=summ/exchangeUSDtoEUR*exchangeUSDtoRUB
+	case currencyChange=="USD" && currencyTarget=="EUR":
+		result=summ*exchangeUSDtoEUR
+	case currencyChange=="USD" && currencyTarget=="RUB":
+		result=summ*exchangeUSDtoRUB
+	case currencyChange=="RUB" && currencyTarget=="EUR":
+		result=summ/exchangeUSDtoRUB*exchangeUSDtoEUR
+	case currencyChange=="RUB" && currencyTarget=="USD":
+		result=summ/exchangeUSDtoRUB
+	}
+	return result
+}
 
-	// currencyChange, currencyTarget, summ := askUser()
+func main () {
+	currencyChange, currencyTarget, summ:= askUser()
+	result := countCurrency(currencyChange, currencyTarget, summ)
 
-	fmt.Println("Конвертация евро в рубль= ", exchangeEURtoRUB)
+	fmt.Printf("Сумма сконвертированной валюты из %s в %s составит %.2f/n", currencyChange, currencyTarget, result)
+
 }
