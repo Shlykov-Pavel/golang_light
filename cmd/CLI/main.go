934a72c03ad437d86f5aa47cb460bb0f3cbf19e1
Shlykovpv <Shlykovpv@kamaz.ru>
2026-02-28 14:51:50 +0300

    3-struct инициализация

---
+++ cmd/CLI/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type BIN struct {
+	id string
+	private bool
+	createdAt time.Time
+	name string
+}
+
+func createBIN()BIN{
+	
+}
+
+func main() {
+
+}
